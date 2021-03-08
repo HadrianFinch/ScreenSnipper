0c0b41d37cae1b25df40f528bb1d21df358c614f
Hadrian Finch <hadrianfinch@outlook.com>
2021-03-08 10:23:00 -0800

    moved into scr folder

---
+++ src/decode.h
@@ -0,0 +1,10 @@
+
+#include <wincodec.h>   // for IWICImagingFactory
+
+HRESULT CreateWICFactory(
+    _Outptr_ IWICImagingFactory** ppFactory);
+
+HRESULT CreateBitmapFromFile(
+    _In_ IWICImagingFactory* pIWICFactory,
+    _In_ PCWSTR uri,
+    _Out_ HBITMAP* phBitmap);
